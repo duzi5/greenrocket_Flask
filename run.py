41e9d518a96755b5aa9db187ce75c82388afab97
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-05-14 16:58:02 -0300

    dsfnjnsjdf

--- run.py
+++ run.py
@@ -1,11 +1,5 @@
 from app import app
-from Alchemy import banco
-from flask import Flask
 
-def create_app(): 
-    app = Flask(__name__)
-    banco.init_app(app)
-    return app
 
 if __name__ == "__main__":  
     app.run()
