89ed5bbe07606d4c7ee2071ddcaea98dab44c0ca
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-05-14 16:52:46 -0300

    lallal

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from app import app
 from Alchemy import banco
+from flask import Flask
 
+def create_app(): 
+    app = Flask(__name__)
+    banco.init_app(app)
+    return app
 
 if __name__ == "__main__":  
-    
-    banco.init_app(app)
-    banco.create_all()
     app.run()
 
