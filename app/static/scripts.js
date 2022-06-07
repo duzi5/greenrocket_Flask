585ce67761d9b2878450bd082938836381d9d754
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-06-07 19:47:34 -0300

    melhorando a responsividade

---
+++ app/static/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // var caixasTexto = $('.linha2');
+    // caixasTexto.css('height', '20px');
+    // $(this).css('height', '100px');
+   
+    var altura = window. screen. height;
+    var largura = window. screen. width;
+
+
+    
+
+
+    if (largura > 1200 ) {
+        $('.card-text').css('font-size', '1.1rem')
+        $('.cardimg').css('width', '300px').css('height', '400px') 
+        $('.card-title').css('font-size', '2rem')
+    } else if (largura > 800 && largura <=1200 ) {
+        $('.card-text').css('font-size', '1.4rem')
+        $('.card-title').css('font-size', '2rem')
+    } else if(largura>300 && largura<=800) { 
+        $('.card-text').css('font-size', '2rem')
+        $('.card-title').css('font-size', '2.5rem')
+        
+    }else{
+        console.log('tá funcionando')
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
