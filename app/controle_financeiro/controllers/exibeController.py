30bb4aba442494cb97c7f00987ff2c9ef3a70b49
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-07-05 14:11:11 -0300

    tocando numeros pelos nomes das cat e meios

--- app/controle_financeiro/controllers/exibeController.py
+++ app/controle_financeiro/controllers/exibeController.py
@@ -30,45 +30,52 @@ def controle_financeiro_home(id, mes,ano):
         totalcategoria = 0
         totalgeral = 0
         percentual = 0
-
+        catClassLista = {}
+        meioClassLista = {}
         for meio in meios: 
             totalm = 0 
-
             meioClass = Meio.query.filter_by(id = meio).first().nome
+            meioClassLista[meio] = meioClass
             for gasto in gastos:
                 if gasto.meio == meio:
                     totalm += gasto.valor
                     totalgeral += gasto.valor
             totalmeio.append( {
                     "meio": meioClass,
-                    "total": totalm
+                    "total": round(totalm, 2)
                     })
                 
 
         categorias = eval(current_user.categorias)
+ 
         for categoria in categorias:
             totalcategoria = 0
             catClass = Categoria.query.filter_by(id = categoria).first().nome  
             catClassId = Categoria.query.filter_by(id = categoria).first().id  
+            catClassLista[catClassId] = catClass
+            
+
             for gasto in gastos: 
-                if gasto.categoria == categoria:
+                if gasto.categoria == catClassId:
                     totalcategoria += gasto.valor 
                 if totalgeral == 0: 
                     percentual = 0
                 else: 
-                    percentual = totalcategoria / totalgeral * 100
+                    percentual = (totalcategoria / totalgeral) * 100
 
             totalcat.append({
                 "id" : catClassId,
                 "categoria" : catClass,
-                "total": totalcategoria, 
-                "percentual" : percentual
+                "total": round(totalcategoria, 2), 
+                "percentual" :  round(percentual, 2)
             })    
+  
+    
 
         print(totalmeio)
         print(totalcat) 
         
+        totalgeral = round(totalgeral,2)
+        
     
-
-    
-    return render_template('gastos.html', totalmeio = totalmeio, totalcat = totalcat, gastos = gastos, categorias = categorias, meios = meios, totalm = totalm, totalcategoria = totalcategoria, ano = ano, mes = mes, totalgeral = totalgeral)
+    return render_template('gastos.html',meioClassLista = meioClassLista, catClassLista = catClassLista, totalmeio = totalmeio, totalcat = totalcat, gastos = gastos, categorias = categorias, meios = meios, totalm = totalm, totalcategoria = totalcategoria, ano = ano, mes = mes, totalgeral = totalgeral)
