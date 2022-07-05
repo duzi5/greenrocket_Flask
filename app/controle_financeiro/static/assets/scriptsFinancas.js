829a75e2baa4a80d60c9c7563e0faff136cd502c
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-07-05 11:26:53 -0300

    visual renovado, cadastro implementado com requireds para evitar erros. gráfico pegando, JS pega a classe e transforma em um array de arrays

--- app/controle_financeiro/static/assets/scriptsFinancas.js
+++ app/controle_financeiro/static/assets/scriptsFinancas.js
@@ -2,39 +2,44 @@ $('#tite').css('color', 'blue')
 
 
 $(window).on("load", function(){
-  var bemEstar= parseFloat(document.getElementById('bem-estar').innerText);
-  var educacao= parseFloat(document.getElementById('educacao').innerText);
-  var transporte= parseFloat(document.getElementById('transporte').innerText);
-  var farmacia= parseFloat(document.getElementById('farmacia').innerText);
-  var tabaco= parseFloat(document.getElementById('tabaco').innerText);
-  var info=parseFloat( document.getElementById('info').innerText);
-  var academia= parseFloat(document.getElementById('academia').innerText);
-  var alimentacao= parseFloat(document.getElementById('alimentacao').innerText);
-  var mercado= parseFloat(document.getElementById('mercado').innerText);
-  var outros= parseFloat(document.getElementById('outros').innerText);
+  // var bemEstar= parseFloat(document.getElementById('bem-estar').innerText);
+  // var educacao= parseFloat(document.getElementById('educacao').innerText);
+  // var transporte= parseFloat(document.getElementById('transporte').innerText);
+  // var farmacia= parseFloat(document.getElementById('farmacia').innerText);
+  // var tabaco= parseFloat(document.getElementById('tabaco').innerText);
+  // var info=parseFloat( document.getElementById('info').innerText);
+  // var academia= parseFloat(document.getElementById('academia').innerText);
+  // var alimentacao= parseFloat(document.getElementById('alimentacao').innerText);
+  // var mercado= parseFloat(document.getElementById('mercado').innerText);
+  // var outros= parseFloat(document.getElementById('outros').innerText);
   
+  var nome = document.getElementsByClassName('nome')
+  var subtotal = document.getElementsByClassName('totalclass')
+  var linha = []
+  var dict = []
+  dict.push(['Task', 'gastos por categoria']) 
+  
+  for (let i = 0; i < nome.length; i++) {
+    linha = [
+      nome[i].innerHTML, parseFloat(subtotal[i].innerHTML) 
+    ]
+    dict.push(linha)
+  }
 
 
+  console.log(dict)
+
+
+
   
+
 
 
 
   google.charts.load("current", {packages:["corechart"]});
   google.charts.setOnLoadCallback(drawChart);
   function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-      ['Task', 'gastos por categoria'],
-      ['Bem-Estar', bemEstar],
-      ['Educação e Softwares', educacao],
-      ['Transporte',  transporte],
-      ['Farmácia', farmacia],
-      ['Tabaco e Cia', tabaco],
-      ['Informática', info],
-      ['Academia', academia],
-      ['Alimentação', alimentacao],
-      ['Mercado', mercado],
-      ['Outros', outros]
-    ]);
+    var data = google.visualization.arrayToDataTable(dict);
   
     var options = {
       title: 'Gastos por categoria',
